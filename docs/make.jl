8b547fe3221c09a9613aabd4f67e12cac2d1ad43
Egor Shmorgun <egor.shmorgun@opensesame.com>
2023-04-04 18:44:48 +0300

    add documentation CI

--- docs/make.jl
+++ docs/make.jl
@@ -16,9 +16,4 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(repo = "github.com/OpenSesame/Elasticsearch.jl.git")
